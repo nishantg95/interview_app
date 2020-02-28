60585ec0ebfd1ca3a5e9f41da215566b2a8bdbaa
adam.boukoungou <adam.boukoungou@AFSWWXJEL6J33E.us.accenturefederal.com>
2020-02-28 15:44:47 -0600

    hfg

---
+++ src/main/java/com/accenture/inteview/InterviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.inteview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/inteview/InterviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.inteview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
