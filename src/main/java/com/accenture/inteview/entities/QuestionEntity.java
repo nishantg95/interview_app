335a358223497b15c5d22527846b113d76977f41
adam.boukoungou <adam.boukoungou@AFSWWXJEL6J33E.us.accenturefederal.com>
2020-03-03 14:24:07 -0600

    changed entities generation type

--- src/main/java/com/accenture/inteview/entities/QuestionEntity.java
+++ src/main/java/com/accenture/inteview/entities/QuestionEntity.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class QuestionEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "TITLE", nullable = false)
--- src/main/java/com/accenture/inteview/entities/TagEntity.java
+++ src/main/java/com/accenture/inteview/entities/TagEntity.java
@@ -24,7 +24,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class TagEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "NAME", nullable = false)
