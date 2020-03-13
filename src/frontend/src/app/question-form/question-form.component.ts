fb0f9bef5a5bdb9ed1e64dc48e6c28c0ef12fda6
nishantg95 <nishant.groveris@gmail.com>
2020-03-13 15:58:30 -0500

    replacing component with updated content

--- src/frontend/src/app/question-form/question-form.component.ts
+++ src/frontend/src/app/question-form/question-form.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
 import { QuestionService } from '../question.service';
 import { Question } from '../interfaces/question';
 import { Tag } from '../interfaces/tag';
 import { TagService } from '../tag.service';
-import { of, Observable, Subscription } from "rxjs";
-import { } from "rxjs";
-//import { Hash } from 'crypto';
-import { concat, BehaviorSubject } from 'rxjs';
-import { stringify } from 'querystring';
-import { map } from 'rxjs/operators';
-import { forkJoin } from 'rxjs';
+import { Observable, of, interval } from 'rxjs';
+import { flatMap, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { QuestionViewComponent } from '../question-view/question-view.component';
+import { TagContentType } from '@angular/compiler';
 
 @Component({
   selector: 'app-questions',
@@ -18,166 +16,66 @@ import { forkJoin } from 'rxjs';
   styleUrls: ['./question-form.component.css']
 })
 export class QuestionFormComponent implements OnInit {
-  questions: Question[];
-  setOfTags: any;
-  tags: Set<Tag> = new Set<Tag>();
-  questionForm = this.fb.group({
-    id: [''],
-    title: ['', Validators.required],
-    body: [''],
-    comment: [''],
-    added_by: [''],
-    tags: ['']
-  });
   constructor(
     private questionService: QuestionService,
     private fb: FormBuilder,
-    private tagService: TagService
-  ) { }
+    private tagService: TagService,
+    private router: Router
+  ) {}
+  questions: Question[];
+  setOfTags: any;
+  newTag: Tag = { id: null, name: null, questions: null };
+  tags: Set<Tag> = new Set<Tag>();
+  key = 'editQuestion';
+  data = this.router[this.key];
+  enableRedirect = false;
+  questionForm = this.fb.group({
+    id: [null],
+    title: ['', Validators.required],
+    body: ['', Validators.required],
+    comment: [''],
+    added_by: ['', Validators.required],
+    tags: ['', Validators.required]
+  });
 
   ngOnInit(): void {
     this.setOfTags = this.tagService.listAllTags();
-    this.questionForm.valueChanges.subscribe(term => {
-      if (term.tags.valueChanges) {
-        console.log(term.tags.pop);
-      }
-    });
+    if (this.data != null && this.router.url === '/editQuestion') {
+      this.questionForm.setValue(this.data);
+
+      console.log(this.router.url);
+    } else if (this.router.url === '/addQuestion') {
+      console.log('we came to play today!');
+    } else {
+      this.router.navigateByUrl('/questions');
+    }
   }
-// addTag(tag : Tag) : Observable<any>{
-
-// }
-
-
-  observables: Observable<String>[] = [];
-  saveNewTags(ques: Question) {
-    let newTagCounter = 0
-    let savedTagCounter = 0;
-    ques.tags.forEach((tag, index) => {
-
-      let tagToBeAdded: Tag;
-      if (tag.id === undefined) {
-        newTagCounter++;
-        tagToBeAdded = { id: null, name: tag.name, questions: null };
-
-        let observable = this.tagService.addTag(tagToBeAdded).pipe(map(
-          newTag => {
-            ques.tags.splice(index, 1);
-            console.log(newTag);
-            ques.tags.push(newTag);
-            console.log('Tag Before', tag);
-            //  this.questionService.addQuestion(ques).subscribe();
-            console.log('Tag After', ques.tags);
-            // return of('in progress');
-            // Route to the newly created question, can get id above
-            return "cool";
-          },
-        ));
-      //  observable.subscribe();
-        this.observables.push(observable);
-      }
-    });
-    // forkJoin(observables)
-    // .subscribe(dataArray => {
-    //     // All observables in `observables` array have resolved and `dataArray` is an array of result of each observable
-    // });
-  }
-
-  // checkIfNewTagAdded(ques: Question): Observable<boolean> {
-  //   let counter: number = 0;
-  //   ques.tags.forEach((tag) => {
-  //     console.log("inside checkIfNewTagAdded  iteration", tag.id)
-  //     if (tag.id === undefined) {
-  //       counter++;
-  //     }
-  //   })
-  //   if (counter > 0)
-  //     return of(true);
-  //   else
-  //     return of(false);
-
-  // }
-  // saveQuestion(ques: Question) {
-  // let allSaved = false;
-  // this.saveNewTags(ques).subscribe({
-  //   next: (x) => {
-  //     allSaved = x;
-  //     console.log(" inside saveQuestionWithNewTag  Next  AllSaved value is ", allSaved);
-  //     this.questionService.addQuestion(ques).subscribe();
-  //   },
-  //   //   if (allSaved) {
-  //   //     console.log("saveQuestionWithNewTag if statement. AllSaved ", allSaved);
-  //   //     this.questionService.addQuestion(ques).subscribe();
-  //   //   }
-  //   // },
-  //   complete: () => {
-  //     console.log(" inside saveQuestionWithNewTag  Complete  AllSaved value is ", allSaved);
-  //     this.questionService.addQuestion(ques).subscribe();
-  //   }
-  // });
-  // else {
-  //   console.log("");
-  // }
-  // }
-
-
-  //this.questionService.addQuestion(ques).subscribe();
-  // this.saveNewTags(ques).subscribe({
-  //   next: (x) => console.log(x),
-  //   complete: () => this.questionService.addQuestion(ques).subscribe()
-  // })
 
   onSubmit() {
-    console.warn(this.questionForm.value);
     const ques: Question = this.questionForm.value;
-    this.saveNewTags(ques);
-    forkJoin(this.observables).subscribe(
-      x => {
-        console.log("inside forkJoin x = " + x);
+    console.log(ques);
+    this.tagService.addTags(ques.tags).subscribe( updatedTags => {
+      ques.tags = updatedTags;
+      if (ques.id === null) {
         this.questionService.addQuestion(ques).subscribe();
+      } else {
+        if (ques !== this.data) {
+          this.questionService.updateQuestion(ques).subscribe();
+        }
       }
-    )
+    });
+
+    this.router[this.key] = undefined;
+    this.router.navigateByUrl('/questions');
+  }
+
+
+  @HostListener('window:beforeunload', ['$event'])
+  @HostListener('window:popstate', ['$event'])
+  doSomething($event) {
+    if (this.questionForm.dirty === true) {
+      this.router[this.key] = undefined;
+      $event.returnValue = true;
+    }
   }
 }
-
-
-    // this.checkIfNewTagAdded(ques).subscribe({
-    //   next: isAdded => {
-    //     if (isAdded) {
-    //       console.log(" new tag check = true.  value of is added", isAdded)
-    //       this.saveQuestion(ques);
-    //     }
-    //     else {
-    //       this.questionService.addQuestion(ques).subscribe();
-    //       console.log(" new tag check = false. value of is added", isAdded)
-    //     }
-    //   }
-    // });
-
-    // this.checkTags(ques);
-
-    // if it exist run ch
-
-    // 
-
-    //
-
-    // let checkTag: Tag;
-    // let newTags: Tag[];
-    // while (ques.tags.length !== 0 ) {
-    //   checkTag = ques.tags.shift();
-    //   if(checkTag.id === undefined){
-    //     const tagToBeAdded = { id: null, name: checkTag.name, questions: null };
-    //     this.tagService.addTag(tagToBeAdded).subscribe(newTag => {
-    //       console.log(newTag);
-    //       ques
-    //     });
-    //   }
-    // }
-    // TODO: check if question was added successfully
-
-// tags: new FormControl([
-//   {
-//     id: 3,
-//     name: 'Java'
-//   }
-// ])
